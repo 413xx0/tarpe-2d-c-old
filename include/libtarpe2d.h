42a588d407e592297b2824bfe77638ecd0eb8d0d
413xx0 <ksalen6school@gmail.com>
2023-04-05 05:01:20 +0300

    Add missed includes into libtarpe2d.h

--- include/libtarpe2d.h
+++ include/libtarpe2d.h
@@ -3,10 +3,17 @@
 
 #include <libtarpe2d/bh86/bh.h>
 #include <libtarpe2d/bh86/quadtree.h>
+
+#include <libtarpe2d/collisions/broad_phase.h>
+#include <libtarpe2d/collisions/narrow_phase.h>
+#include <libtarpe2d/collisions/solve_collision.h>
+
 #include <libtarpe2d/config.h>
+#include <libtarpe2d/math_utils.h>
 #include <libtarpe2d/rb_shape.h>
 #include <libtarpe2d/rb_shape_array.h>
 #include <libtarpe2d/rigidbody.h>
+#include <libtarpe2d/shape.h>
 #include <libtarpe2d/tick.h>
 #include <libtarpe2d/vec2.h>
 
