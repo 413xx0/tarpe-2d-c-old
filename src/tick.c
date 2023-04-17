8c04e87e8b1078738a899432723351985fd6f3d6
413xx0 <ksalen6school@gmail.com>
2023-04-17 19:02:04 +0300

    Fix collision resolution impulses calculation

--- src/tick.c
+++ src/tick.c
@@ -6,18 +6,27 @@
 #include <stdint.h>
 
 
-static inline void tick_rigidbody(struct rigidbody * rb, float_t dt)
+static inline void tick_velocities(struct rigidbody * rb, float_t dt)
 {
 	struct vec2 tmp;
 	vec2_mul(&rb->tick_force, rb->inv_mass * dt, &tmp); // tmp = a * dt
 
 	vec2_add(&rb->linear_velocity, &tmp, &rb->linear_velocity); // v += a * dt
 
+	float_t angular_acceleration = rb->torque / rb->inv_moment_of_inertia;
+	rb->angular_velocity += angular_acceleration * dt;
+
+	vec2_nullify(&rb->tick_force);
+	rb->torque = 0;
+}
+
+static inline void tick_pos_angle(struct rigidbody * rb, float_t dt)
+{
+	struct vec2 tmp;
+
 	vec2_mul(&rb->linear_velocity, dt, &tmp); // tmp = v * dt
 	vec2_add(&rb->pos, &tmp, &rb->pos); // p =+ v * dt
 
-	float_t angular_acceleration = rb->torque / rb->inv_moment_of_inertia;
-	rb->angular_velocity += angular_acceleration * dt;
 	rb->angle += rb->angular_velocity * dt;
 
 	vec2_nullify(&rb->tick_force);
@@ -29,12 +38,18 @@ int tarpe_tick(struct rbs_array * bodies, float_t dt)
 	int err;
 	if ((err = bh86_apply_gravity_forces(bodies))) return err;
 
+	for (struct rb_shape_base ** rbs_ptr = bodies->shapes; rbs_ptr < bodies->shapes + bodies->size;
+	     ++rbs_ptr)
+	{
+		tick_velocities((struct rigidbody *)(*rbs_ptr), dt);
+	}
+
 	if ((err = sap_cd_broad_phase(bodies))) return err;
 
 	for (struct rb_shape_base ** rbs_ptr = bodies->shapes; rbs_ptr < bodies->shapes + bodies->size;
 	     ++rbs_ptr)
 	{
-		tick_rigidbody((struct rigidbody *)(*rbs_ptr), dt);
+		tick_pos_angle((struct rigidbody *)(*rbs_ptr), dt);
 
 		if ((*rbs_ptr)->type == TARPE__SHAPE_TYPE__RECTANGLE)
 		{
